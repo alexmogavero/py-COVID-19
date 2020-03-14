052eeec977a2069caee123184a7487dc4bd1a445
Alessandro Mogavero <alexmogavero@users.noreply.github.com>
2020-03-14 16:58:48 +0100

    implemented plot of more regions on the same axis

--- read_data.py
+++ read_data.py
@@ -11,7 +11,7 @@ DATA_DIR = "/home/oslo/Software/COVID-19/dati-json/"
 FILE_STATO = "dpc-covid19-ita-andamento-nazionale.json"
 FILE_REG = "dpc-covid19-ita-regioni.json"
 
-def plot_data(data_dict, shift=0):
+def plot_data(data_dict, shift=0, ax=None):
     t = [parser.parse(dt['data']) for dt in data_dict]
     y = [dt['totale_casi']-shift for dt in data_dict]
     
@@ -19,15 +19,18 @@ def plot_data(data_dict, shift=0):
     t_fit = t + [t[-2] + timedelta(days=tt) for tt in t_fit]
     fit_label, y_fit = fit_data(t,y,t_fit)
     
-    pyplot.figure()
-    ax = pyplot.axes()
-    pyplot.plot_date(t,y)
-    pyplot.plot_date(t_fit,y_fit,fmt='-',label=fit_label)
-    formatter = dates.DateFormatter('%d/%m')
-    ax.xaxis.set_major_formatter(formatter)
+    if ax is None:
+        pyplot.figure()
+        ax = pyplot.axes()
+        pyplot.xlabel('Data')
+        pyplot.ylabel('totale casi')
+        formatter = dates.DateFormatter('%d/%m')
+        ax.xaxis.set_major_formatter(formatter)
+        
+    ax.plot_date(t,y)
+    ax.plot_date(t_fit,y_fit,fmt='-',label=fit_label)
     ax.legend()
-    pyplot.xlabel('Data')
-    pyplot.ylabel('totale casi')
+    
     
 def exp_fun(t,tau,i0):
     return i0*np.exp(t/tau)
@@ -46,12 +49,20 @@ def fit_data(t,y,t_fit):
     label = 'tau={:.2f} t0={:.2f}'.format(popt[0],t0)
     return label, y_fit
     
-def plot_regione(data_dict, nome_reg):
-    if nome_reg=='Lazio':
-        plot_data([d for d in data_dict if d['denominazione_regione']==nome_reg], shift=3)
-    else:
-        plot_data([d for d in data_dict if d['denominazione_regione']==nome_reg])
+def plot_regione(data_dict, reg):
+    if not isinstance(reg, list):
+        reg = [reg]
         
+    pyplot.figure()
+    ax = pyplot.axes()
+    ax.set_prop_cycle(color=['b','b','r','r','g','g','m','m','c','c','k','k'])
+    for nome_reg in reg:
+        if nome_reg=='Lazio':
+            plot_data([d for d in data_dict if d['denominazione_regione']==nome_reg],
+                    shift=3, ax=ax)
+        else:
+            plot_data([d for d in data_dict if d['denominazione_regione']==nome_reg], ax=ax)
+            
     pyplot.title(nome_reg)
     
 def plot_stato(data_dict):
@@ -65,7 +76,7 @@ if __name__=="__main__":
     with open(path.join(DATA_DIR,FILE_STATO), 'r') as f:
         data_stato = json.load(f)
         
-    plot_regione(data_reg, 'Campania')
+    plot_regione(data_reg, ['Campania', 'Lazio'])
     
     plot_regione(data_reg, 'Lazio')
     
